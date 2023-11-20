ef55a0e49f0709099d1f06c3afe95fe79a33705c
hogang <hyogangster1@gmail.com>
2023-11-20 20:20:16 +0900

    feat - #1: SPM-SnapKit 추가, 관심주식목록 ViewController 생성

---
+++ orange-stock-ios/orange-stock-ios/View/FavoriteStockListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteStockListViewController.swift
+//  orange-stock-ios
+//
+//  Created by hogang on 2023/11/20.
+//
+
+import UIKit
+import SnapKit
+
+class FavoriteStockListViewController: UIViewController {
+    
+    // MARK: Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

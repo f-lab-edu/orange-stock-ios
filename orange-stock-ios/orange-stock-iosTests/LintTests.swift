5e4e090c7a866c3f7020d16b74b899ef833f3bcf
hogang <hyogangster1@gmail.com>
2024-01-07 15:47:17 +0900

    test: github action swift lint test

---
+++ orange-stock-ios/orange-stock-iosTests/LintTests.swift
@@ -0,0 +1,23 @@
+//
+//  LintTests.swift
+//  orange-stock-iosTests
+//
+//  Created by hogang on 2024/01/07.
+//
+
+import Foundation
+
+import XCTest
+
+final class LintTests: XCTestCase {
+    
+    // MARK: Lint test
+    
+    func testLint() {
+        let test:String = ""
+        
+        if test == "" {
+            test = "qaskjdfjasgjksajlkdgjksdjdglksajklgjklasjgkljrwoigjoaj gslkdjklsaj gj waoigjklasjdgkljsalkdgjklsajgl;as jlkdgjsalkjgklsa jglkj woigjalkjskldgjasoksjgoiajglasdgkljoijgoalmlkahr"
+        }
+    }
+}
